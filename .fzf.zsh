3a84b1f94d29e908e79048203331feb7516c16cf
taku <shishido_takuya@xhift.com>
2021-02-22 10:31:51 +0900

    fzf: modify: fzf path

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,16 @@
 # Setup fzf
+
+FZFHOME=/opt/homebrew/opt/fzf
+
 # ---------
-if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+if [[ ! "$PATH" == *$FZFHOME/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$FZFHOME/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$FZFHOME/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+source "$FZFHOME/shell/key-bindings.zsh"
